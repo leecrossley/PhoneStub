cab9b7dd2bf581c452e20e157e35d3a5fd51ea9f
Lee Crossley <leee@hotmail.co.uk>
2012-05-24 16:07:04 +0100

    browser error handler

--- cordova-stub-tests.js
+++ cordova-stub-tests.js
@@ -2,9 +2,12 @@ var Browser = require("zombie");
 var assert = require("assert");
 
 var browser = new Browser()
-browser.visit("file:///_src/Cordova-Stub/index.html", function () {
 
+browser.on("error", function(error) {
+	console.error(error);
+});
+
+browser.visit("file:///_src/Cordova-Stub/index.html", function() {
 	assert.ok(browser.success);
 	assert.equal(browser.text("title"), "Cordova Stub Tests");
-
 });
