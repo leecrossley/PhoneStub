c4ba011e0b67ee914a4793560bc0157408078059
Lee Crossley <leee@hotmail.co.uk>
2012-05-24 15:27:33 +0100

    basic zombie test for framwork

---
+++ cordova-stub-tests.js
@@ -0,0 +1,10 @@
+var Browser = require("zombie");
+var assert = require("assert");
+
+var browser = new Browser()
+browser.visit("file:///_src/Cordova-Stub/index.html", function () {
+
+	assert.ok(browser.success);
+	assert.equal(browser.text("title"), "Cordova Stub Tests");
+
+});
