a93afaddd073d716620d600fa19b5da310126f79
Lee Crossley <leee@hotmail.co.uk>
2012-05-29 16:11:18 +0100

    removed packages

--- cordova-stub-tests.js
+++ cordova-stub-tests.js
@@ -1,7 +1,7 @@
-var Browser = require("zombie");
+var zombie = require("zombie");
 var assert = require("assert");
 
-var browser = new Browser()
+var browser = new Browser();
 
 Browser.debug = true;
 
@@ -9,7 +9,8 @@ browser.on("error", function(error) {
 	console.error(error);
 });
 
-browser.visit("file:///_src/Cordova-Stub/index.html", function() {
-	assert.ok(browser.success);
-	assert.equal(browser.text("title"), "Cordova Stub Tests");
+browser.visit("http://localhost:3000/", function() {
+	//assert.ok(browser.success);
+	//assert.equal(browser.text("title"), "Cordova Stub Tests");
+	console.log(browser.text("title"));
 });
