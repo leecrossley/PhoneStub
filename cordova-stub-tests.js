9b48406b1b04c1f3163646f2b8faba284e889d85
Lee Crossley <leee@hotmail.co.uk>
2012-05-30 15:03:54 +0100

    linted

--- cordova-stub-tests.js
+++ cordova-stub-tests.js
@@ -1,17 +1,20 @@
+/*global require, console, process */
+/*jslint sloppy: true */
+
 var http = require("http");
 var fs = require("fs");
 var path = require("path");
-var zombie = require("zombie");
+var Zombie = require("zombie");
 var assert = require("assert");
 
 http.createServer(function (request, response) {
 	if (request.url === "/") {
 		request.url = "/index.html";
 	}
-	var filePath = "." + request.url;
-	var fileExt = path.extname(filePath);
-	var contentType;
-	
+	var filePath = "." + request.url,
+		fileExt = path.extname(filePath),
+		contentType;
+
 	if (fileExt === ".js") {
 		contentType = "text/javascript";
 	} else if (fileExt === ".css") {
@@ -19,38 +22,34 @@ http.createServer(function (request, response) {
 	} else {
 		contentType = "text/html";
 	}
-	 
-	path.exists(filePath, function(exists) {
+
+	path.exists(filePath, function (exists) {
 		if (exists) {
-			fs.readFile(filePath, function(error, content) {
+			fs.readFile(filePath, function (error, content) {
 				if (error) {
 					response.writeHead(500);
 					response.end();
-				}
-				else {
+				} else {
 					response.writeHead(200, { "Content-Type": contentType });
 					response.end(content, "utf-8");
 				}
 			});
-		}
-		else {
+		} else {
 			response.writeHead(404);
 			response.end();
 		}
 	});
 }).listen(8125);
 
-zombie.debug = true;
+var browser = new Zombie();
 
-var browser = new zombie();
-
-browser.on("error", function(error) {
+browser.on("error", function (error) {
 	console.error(error);
 });
 
-browser.visit("http://localhost:8125/", function() {
+browser.visit("http://localhost:8125/", function () {
 	assert.ok(browser.success);
 	assert.equal(browser.text("title"), "Cordova Stub Tests");
-	console.log("All tests passed.")
+	console.log("All tests passed.");
 	process.exit();
 });
