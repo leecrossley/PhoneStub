b30ad32e9208ddbf26ef333d175b4fe6c4477409
Lee Crossley <leee@hotmail.co.uk>
2012-05-30 18:23:36 +0100

    extracted server to package

--- cordova-stub-tests.js
+++ cordova-stub-tests.js
@@ -1,45 +1,11 @@
 /*global require, console, process */
 /*jslint sloppy: true */
 
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
 var Zombie = require("zombie");
 var assert = require("assert");
+var server = require("realsimpleserver");
 
-http.createServer(function (request, response) {
-	if (request.url === "/") {
-		request.url = "/index.html";
-	}
-	var filePath = "." + request.url,
-		fileExt = path.extname(filePath),
-		contentType;
-
-	if (fileExt === ".js") {
-		contentType = "text/javascript";
-	} else if (fileExt === ".css") {
-		contentType = "text/css";
-	} else {
-		contentType = "text/html";
-	}
-
-	path.exists(filePath, function (exists) {
-		if (exists) {
-			fs.readFile(filePath, function (error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				} else {
-					response.writeHead(200, { "Content-Type": contentType });
-					response.end(content, "utf-8");
-				}
-			});
-		} else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-}).listen(8125);
+server.spinUp(8125);
 
 var browser = new Zombie();
 
